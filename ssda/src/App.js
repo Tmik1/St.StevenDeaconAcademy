96a48d438083db6038d9878a2745a0084d001a41
TMIK <59984638+Tmik1@users.noreply.github.com>
2021-04-08 23:23:17 -0500

    Refresh

---
+++ ssda/src/App.js
@@ -0,0 +1,126 @@
+import logo from "./logo.svg";
+import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar>
+        <Toolbar className="Top-Bar">
+          <Typography
+            className="Top-txt"
+            style={{ flexGrow: 1, fontFamily: "futura", fontSize: 20 }}
+          >
+            St. Steven's Deacon Academy
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Paper className="Paper-M">
+          <Paper>
+            <Card className="Card-M">
+              <CardActionArea>
+                <CardMedia className="Card-img" image="/logo.svg" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Lizard
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Paper>
+
+          <Paper>
+            <Card className="Card-M">
+              <CardActionArea>
+                <CardMedia className="Card-img" image="/logo.svg" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Lizard
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Paper>
+
+          <Paper>
+            <Card className="Card-M">
+              <CardActionArea>
+                <CardMedia className="Card-img" image="/logo.svg" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Lizard
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Paper>
+        </Paper>
+      </header>
+    </div>
+  );
+}
+
+export default App;
